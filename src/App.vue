bc5782f1bc413ff51c5a72959b757dee43db02d8
lucas.andrade <lucasgonaff@outlook.com>
2022-06-24 16:36:32 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <h1>Hello world</h1>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const App = defineComponent({
+});
+
+export default App;
+</script>
+
+<style>
+
+</style>
