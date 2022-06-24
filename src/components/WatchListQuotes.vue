20b9302674e6404436c73482699e1db10a30ad5e
lucas.andrade <lucasgonaff@outlook.com>
2022-06-24 19:57:31 -0300

    add component watchlist quotes

---
+++ src/components/WatchListQuotes.vue
@@ -0,0 +1,18 @@
+<template>
+    <ListQuotes/>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import ListQuotes from './ListQuotes.vue';
+
+const WatchListQuotes = defineComponent({
+    components : {ListQuotes}
+});
+
+export default WatchListQuotes;
+</script>
+
+<style>
+
+</style>
