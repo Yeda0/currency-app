e062e435819dd79f9803c4f3b64327278b48f06c
lucas.andrade <lucasgonaff@outlook.com>
2022-06-24 17:22:27 -0300

    adicionando estado das moedas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="container grid-lg">
-    <h1>Hello world</h1>
+    <input v-model="test">
+    <h1>{{quotes}}</h1>
   </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent, ref,reactive, toRefs, onMounted } from 'vue';
+import api from './services/api'
 
 const App = defineComponent({
+  setup() {
+    const data = reactive({
+      quotes : {},
+      test : ''   
+     
+    })
+
+    onMounted(async() => {
+      const response =  await api.all()
+      data.quotes = response.data
+    })
+    
+    return {...toRefs(data)}
+  }
 });
 
 export default App;
