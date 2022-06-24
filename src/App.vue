22ec346fe8b1d6add6e421e025aa2869eb5a6908
lucas.andrade <lucasgonaff@outlook.com>
2022-06-24 20:37:31 -0300

    watchlist modifed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container grid-lg my-2 py-2">
+
+    <div class="card mb-2">
+      <div class="card-header">
+        <div class="h4">Acompanhando</div>
+      </div>
+      <div class="card-body">
+        <WatchListQuotes/>
+      </div>
+    </div>
+
     <div class="card">
       <div class="card-header">
         <div class="h4">
@@ -17,10 +27,11 @@
 import { defineComponent, ref,reactive, toRefs, onMounted } from 'vue';
 import api from './services/api'
 import ListQuotes from './components/ListQuotes.vue';
+import WatchListQuotes from './components/WatchListQuotes.vue';
 
 
 const App = defineComponent({
-  components : {ListQuotes},
+  components : {ListQuotes, WatchListQuotes},
   setup() {
     const data = reactive({
       quotes : {},
--- src/components/WatchListQuotes.vue
+++ src/components/WatchListQuotes.vue
@@ -1,13 +1,25 @@
 <template>
-    <ListQuotes/>
+    <ListQuotes :quotes="quotes"/>
+    <div class="mt-2 text-right">
+         <cite class="text-small">
+        Atualizará novamente em  <b>{{nextUpdateTime}} segundos</b>
+        </cite>
+    </div>
+   
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import ListQuotes from './ListQuotes.vue';
 
 const WatchListQuotes = defineComponent({
-    components : {ListQuotes}
+    components : {ListQuotes},
+
+    setup() {
+        const quotes = ref({});
+        const nextUpdateTime = ref(30)
+        return {quotes , nextUpdateTime} 
+    }
 });
 
 export default WatchListQuotes;
